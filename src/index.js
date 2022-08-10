cab64af6234107ed0b0b6f5f685b7bd4814217e0
Detrich Utti <detrich.utti1@t-mobile.com>
2022-08-10 12:58:59 -0500

    feat: basic_request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { subscribe, request } from './components/core/qlkubeRequests';
+export { subscribe, basic_request } from './components/core/qlkubeRequests';
 export { QlkubeProvider, QlkubeContext } from './components/react/context/QlkubeProvider';
 export { useSub, useMonoSub, useLink } from './components/react/hooks/qlkubeHooks';
