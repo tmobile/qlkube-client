503b8a0662c82028c9addb2515b3db647b0cb6cc
Detrich Utti <detrich.utti1@t-mobile.com>
2023-04-13 17:06:31 -0500

    fix: get to post

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -498,29 +498,35 @@ var subscribe = /*#__PURE__*/function () {
 }();
 var httpQuery = /*#__PURE__*/function () {
   var _ref2 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee2(qlkubeUrl, queryString, token, queryVariables) {
-    var config, result;
+    var body, config, result;
     return _regeneratorRuntime().wrap(function _callee2$(_context2) {
       while (1) switch (_context2.prev = _context2.next) {
         case 0:
+          _context2.prev = 0;
+          body = {
+            query: queryString,
+            variables: queryVariables
+          };
           config = {
             headers: {
-              connectionParams: JSON.stringify({
-                authorization: "Bearer ".concat(token),
-                query: queryString,
-                variables: queryVariables
-              })
+              'Content-Type': 'application/json',
+              authorization: "Bearer ".concat(token)
             }
           };
-          _context2.next = 3;
-          return axios__default["default"].get(qlkubeUrl, _objectSpread2({}, config));
-        case 3:
+          _context2.next = 5;
+          return axios__default["default"].post(qlkubeUrl, _objectSpread2({}, body), _objectSpread2({}, config));
+        case 5:
           result = _context2.sent;
           return _context2.abrupt("return", result.data);
-        case 5:
+        case 9:
+          _context2.prev = 9;
+          _context2.t0 = _context2["catch"](0);
+          console.log('err', _context2.t0);
+        case 12:
         case "end":
           return _context2.stop();
       }
-    }, _callee2);
+    }, _callee2, null, [[0, 9]]);
   }));
   return function httpQuery(_x10, _x11, _x12, _x13) {
     return _ref2.apply(this, arguments);
